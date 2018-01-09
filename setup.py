d87fdf67f0b6db88cc9239e4b48940db2752ad4c
FHA <elric91@users.noreply.github.com>
2018-01-10 00:21:11 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyzigate',
+  packages = ['pyzigate'],
+  version = '0.1',
+  description = 'Interface library for ZiGate (http://zigate./fr)',
+  author = 'Frédéric HARS & ISO-B',
+  author_email = 'frederic.hars@gmail.com',
+  url = 'https://github.com/elric91/ZiGate',
+  download_url = 'https://github.com/elric91/ZiGate/archive/0.1.tar.gz',
+  keywords = ['zigate', 'zigbee', 'python3']
+  classifiers = [],
+)
