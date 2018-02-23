b986795b914fb675b00503e49623b412d1f3bf3b
Julien Bouyoud <jul86bouyd@gmail.com>
2018-02-23 23:04:13 +0100

    feat(zigate): add commands & message handler
    (cherry picked from commit 4734ac7)

--- pyzigate/features/zigate_feature.py
+++ pyzigate/features/zigate_feature.py
@@ -19,7 +19,7 @@ class Feature(AbstractFeature):
         if msg_type == b'8000':
             struct = OrderedDict([('status', 'int'), ('sequence', 8),
                                   ('packet_type', 16), ('info', 'rawend')])
-            msg = self.decode_struct(struct, msg_data)
+            msg = zigate.decode_struct(struct, msg_data)
 
             status_codes = {0: 'Success', 1: 'Invalid parameters',
                             2: 'Unhandled command', 3: 'Command failed',
@@ -42,7 +42,7 @@ class Feature(AbstractFeature):
             zgt_log_levels = ['Emergency', 'Alert', 'Critical', 'Error',
                               'Warning', 'Notice', 'Information', 'Debug']
             struct = OrderedDict([('level', 'int'), ('info', 'rawend')])
-            msg = self.decode_struct(struct, msg_data)
+            msg = zigate.decode_struct(struct, msg_data)
 
             ZGT_LOG.debug('RESPONSE 8001 : Log Message')
             ZGT_LOG.debug('  - Log Level : {}'.format(zgt_log_levels[msg['level']]))
@@ -50,12 +50,60 @@ class Feature(AbstractFeature):
 
             return True
 
+        # Network State response
+        if msg_type == b'8009':
+            struct = OrderedDict([('short_addr', 16),
+                                  ('ext_addr', 64),
+                                  ('pan_id', 16),
+                                  ('extpan_id', 64),
+                                  ('channel', 8)])
+            msg = zigate.decode_struct(struct, msg_data)
+
+            ZGT_LOG.debug('RESPONSE : Network State response')
+            ZGT_LOG.debug('  - Short Address    : {}'.format(msg['short_addr']))
+            ZGT_LOG.debug('  - Extended Address : {}'.format(msg['ext_addr']))
+            ZGT_LOG.debug('  - PAN ID           : {}'.format(msg['pan_id']))
+            ZGT_LOG.debug('  - Extended PAN ID  : {}'.format(msg['extpan_id']))
+            ZGT_LOG.debug('  - Channel          : {}'.format(msg['channel']))
+            return True
+
+        # Version List
+        if msg_type == b'8010':
+            struct = OrderedDict([('major', 'int16'), ('installer', 'int16')])
+            msg = zigate.decode_struct(struct, msg_data)
+
+            ZGT_LOG.debug('RESPONSE : Version List')
+            ZGT_LOG.debug('  - Major version     : {}'.format(msg['major']))
+            ZGT_LOG.debug('  - Installer version : {}'.format(msg['installer']))
+
+            return True
+
+        # Network joined / formed
+        if msg_type == b'8024':
+            struct = OrderedDict([('status', 8), ('short_addr', 16), ('extended_addr', 64), ('channel', 8)])
+            msg = zigate.decode_struct(struct, msg_data)
+
+            status = 'Unknown'
+            if msg['status'] == b'00':
+                status = 'Joined existing network'
+            elif msg['status'] == b'01':
+                status = 'Formed new network'
+            elif b'80' <= msg['status'] <= b'F4':
+                status = 'Failed (ZigBee event codes)'
+
+            ZGT_LOG.debug('RESPONSE : Network joined / formed')
+            ZGT_LOG.debug('  - Status            : {}'.format(status))
+            ZGT_LOG.debug('  - Short Address     : {}'.format(msg['short_addr']))
+            ZGT_LOG.debug('  - Extended Address  : {}'.format(msg['extended_addr']))
+            ZGT_LOG.debug('  - Channel           : {}'.format(msg['channel']))
+            return True
+
         # Default Response
-        elif msg_type == b'8101':
+        if msg_type == b'8101':
             struct = OrderedDict([('sequence', 8), ('endpoint', 8),
                                   ('cluster', 16), ('command_id', 8),
                                   ('status', 8)])
-            msg = self.decode_struct(struct, msg_data)
+            msg = zigate.decode_struct(struct, msg_data)
 
             ZGT_LOG.debug('RESPONSE 8101 : Default Response')
             ZGT_LOG.debug('  - Sequence       : {}'.format(msg['sequence']))
@@ -67,23 +115,12 @@ class Feature(AbstractFeature):
 
             return True
 
-        # Version List
-        if msg_type == b'8010':
-            struct = OrderedDict([('major', 'int16'), ('installer', 'int16')])
-            msg = self.decode_struct(struct, msg_data)
-
-            ZGT_LOG.debug('RESPONSE : Version List')
-            ZGT_LOG.debug('  - Major version     : {}'.format(msg['major']))
-            ZGT_LOG.debug('  - Installer version : {}'.format(msg['installer']))
-
-            return True
-
         # APS Data Confirm Fail
         if msg_type == b'8702':
             struct = OrderedDict([('status', 8), ('src_endpoint', 8),
                                   ('dst_endpoint', 8), ('dst_address_mode', 8),
                                   ('dst_address', 64), ('sequence', 8)])
-            msg = self.decode_struct(struct, msg_data)
+            msg = zigate.decode_struct(struct, msg_data)
 
             ZGT_LOG.debug('RESPONSE 8702 : APS Data Confirm Fail')
             ZGT_LOG.debug('  - Status         : {}'.format(msg['status']))
@@ -95,7 +132,6 @@ class Feature(AbstractFeature):
 
             return True
 
-        # TODO 0x8009
         return False
 
 
@@ -144,19 +180,47 @@ class CommandsMixin:
         """
         self.send_data("0013")
 
-# Zigate -> Obj	0x0020	Set Expended PANID	<64-bit Extended PAN ID:uint64_t>
-# Zigate -> Obj	0x0021	Set Channel Mask	<channel mask:uint32_t>
-# Zigate -> Obj	0x0022	Set Security State + Key	<key type: uint8_t>
-# <key: data>
-# Zigate -> Obj	0x0023	Set device Type	<device type: uint8_t>
-# Device Types:
-# 0 = Coordinator
-# 1 = Router
-# 2 = Legacy Router
-# Zigate -> Obj	0x0024	Start Network
-# ==> Network Joined / Formed
-# Zigate -> Obj	0x0025	Start Network Scan
-# ==> Network Joined / Formed
-# Zigate -> Obj	0x0027	Enable Permissions Controlled Joins	<Enable/Disable : uint8_t>	Status
-# 1 – Enable
-# 2 – Disable
+    def set_expanded_panid(self, expanded_pan_id):
+        """
+        Set Expended PANID
+
+        :type self: Zigate
+        :type expanded_pan_id: uint64_t
+        """
+        self.send_data("0020", '{:08x}'.format(expanded_pan_id))
+
+    def set_channel(self, channel_mask):
+        """
+        Set Channel Mask
+
+        :type self: Zigate
+        :type channel_mask: uint32_t
+        """
+        self.send_data("0021", '{:04x}'.format(channel_mask))
+
+    def set_device_type(self, device_type):
+        """
+        Set Channel Mask
+
+        :type self: Zigate
+        :type device_type: DeviceType
+        """
+        self.send_data("0021", '{:02x}'.format(device_type))
+
+    def start_network(self, scan=False):
+        """
+        Start Network
+
+        :type self: Zigate
+        :type scan: bool
+        """
+        self.send_data("0025" if scan else "0024")
+
+    def switch_permission_controlled_join(self, permission_controlled_join=True):
+        """
+        Start Network
+
+        :type self: Zigate
+        :type permission_controlled_join: bool
+        """
+        self.send_data("0027", '{:02x}'.format(1 if permission_controlled_join else 2))
--- pyzigate/states.py
+++ pyzigate/states.py
@@ -1,7 +1,15 @@
 from enum import IntEnum, unique
 
+
 @unique
 class OnOffState(IntEnum):
     ON = 1
     OFF = 0
     TOGGLE = 2
+
+
+@unique
+class DeviceType(IntEnum):
+    COORDINATOR = 0
+    ROUTER = 1
+    LEGACY_ROUTER = 2
