867a16ee765046da3fa9fb2489d246618c564000
FHA <elric91@users.noreply.github.com>
2018-03-18 22:36:33 +0100

    bump to 0.1.3.post1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='pyzigate',
     packages=['pyzigate'],
-    version='0.1.2',
+    version='0.1.3.post1',
     description='Interface library for ZiGate (http://zigate./fr)',
     author='Frédéric HARS & Vesa YLIKYLÄ',
     author_email='frederic.hars@gmail.com',
     url='https://github.com/elric91/ZiGate',
-    download_url='https://github.com/elric91/ZiGate/archive/v0.1.2.tar.gz',
+    download_url='https://github.com/elric91/ZiGate/archive/v0.1.3.tar.gz',
     keywords=['zigate', 'zigbee', 'python3'],
     classifiers=[
         'Development Status :: 4 - Beta',
