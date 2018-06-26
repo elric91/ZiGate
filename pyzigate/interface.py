257003b9763c05e93dbfdaa6e124df5619038338
root <root@ubuntu.numericable.fr>
2018-06-26 11:09:17 +0200

    remove useless logs

--- pyzigate/interface.py
+++ pyzigate/interface.py
@@ -323,7 +323,6 @@ class ZiGate(commands_helpers.Mixin, attributes_helpers.Mixin):
                 ZGT_LOG.debug('  - IEEE         : {}'.format(msg['IEEE']))
                 ZGT_LOG.debug('  - Power Source : {}'.format(msg['power_source']))
                 ZGT_LOG.debug('  - Link Quality : {}'.format(msg['link_quality']))
-                ZGT_LOG.debug(' TEST 21h53')
                 if int(msg['link_quality']) != 255:
                     #Found enpoint
                     for device in self._known_devices:
