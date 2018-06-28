1aa3878c039b154bb4466bc6dd14620b410b304d
root <root@ubuntu.numericable.fr>
2018-06-28 17:21:22 +0200

    update the 'last seen' value when the RSSI is updated in order to push automatically the new value to grafana / influxdb ( instead of waiting the next status change )

--- pyzigate/interface.py
+++ pyzigate/interface.py
@@ -330,10 +330,12 @@ class ZiGate(commands_helpers.Mixin, attributes_helpers.Mixin):
                         if str(device[:4]) == msg['addr'].decode('UTF-8'):
                             # Update the device with Link Quality value
                             self.set_device_property(msg['addr'], device[4:6].encode(), "Link_quality", '{}'.format(msg['link_quality']))
+                            self.set_device_property(msg['addr'], device[4:6].encode(), ZGT_LAST_SEEN, strftime('%Y-%m-%d %H:%M:%S'))
 
                 else:
                     ZGT_LOG.error('{} dead ? '.format(msg['ID']))
                     self.set_device_property(msg['addr'], device[4:6].encode(), "Link_quality", '0')
+                    self.set_device_property(msg['addr'], device[4:6].encode(), ZGT_LAST_SEEN, strftime('%Y-%m-%d %H:%M:%S'))
 
                 if len(msg['next']) < 13:
                     break
